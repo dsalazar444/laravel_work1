bde807171f9738622aca5f4e4359c3d91b34360a
Daniela Salazar <dsalazara1@eafit.edu.co>
2026-02-23 16:54:03 -0500

    Código funcional workshop 2

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,24 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
     # Atributo que es una lista publica
     # de acá es donde se sacan los productos de la web, de momento no son como bd, sino esto
- public static $products = [
- ["id"=>"1", "name"=>"TV", "description"=>"Best TV", "price"=>"$10"],
- ["id"=>"2", "name"=>"iPhone", "description"=>"Best iPhone", "price"=>"$10"],
- ["id"=>"3", "name"=>"Chromecast", "description"=>"Best Chromecast", "price"=>"$10"],
- ["id"=>"4", "name"=>"Glasses", "description"=>"Best Glasses", "price"=>"$10"]
- ];
+
 
  public function index(): View
  {
  $viewData = [];
  $viewData["title"] = "Products - Online Store";
  $viewData["subtitle"] = "List of products";
- $viewData["products"] = ProductController::$products;
+ $viewData["products"] = Product::all();
  return view('product.index')->with("viewData", $viewData);
  }
  
@@ -29,7 +25,7 @@ class ProductController extends Controller
  public function show(string $id) : View
  {
  $viewData = [];
- $product = ProductController::$products[$id-1];
+ $product = Product::findOrFail($id);
  $viewData["title"] = $product["name"]." - Online Store";
  $viewData["subtitle"] = $product["name"]." - Product information";
  $viewData["product"] = $product;
@@ -47,14 +43,17 @@ class ProductController extends Controller
  }
 
  # acá es donde llegan los datos desde view, pues se manda metodo post, o sea, un request
- public function save(Request $request)
+ public function save(Request $request): \Illuminate\Http\RedirectResponse
  {
  $request->validate([
  "name" => "required",
  "price" => "required"
  ]);
- dd($request->all()); #Mostramos todos los datos enviados en la peticion
+
  //here will be the code to call the model and save it to the database
+ Product::create($request->only(["name","price"]));
+ return back();
+ 
  }
 } 
 
---
+++ app/Models/Product.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Comment;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Collection;
+
+// models are PHP classes that represent and interact with your database tables using the built-in Eloquent ORM.
+
+class Product extends Model
+{
+    use HasFactory;
+    /**
+    * PRODUCT ATTRIBUTES
+    * $this->attributes['id'] - int - contains the product primary key (id)
+    * $this->attributes['name'] - string - contains the product name
+    * $this->attributes['price'] - int - contains the product price
+    * $this->comments - Comment[] - contains the associated comments
+    */
+
+    use HasFactory;
+/**
+* PRODUCT ATTRIBUTES
+* $this->attributes['id'] - int - contains the product primary key (id)
+* $this->attributes['name'] - string - contains the product name
+* $this->attributes['price'] - int - contains the product price
+* $this->comments - Comment[] - contains the associated comments
+*/
+    public function comments(): HasMany { return $this->hasMany(Comment::class); } //A product can have many comments -> use product_id in Comments to connect
+
+    public function getComments(): Collection { return $this->comments; }
+    public function setComments(Collection $comments): void { $this->comments = $comments; }
+
+    //contiene los atributos que se pueden llenar al mismo tiempo (sino tocaría cada atributo en una linea dif)
+    protected $fillable = ['name','price'];
+
+    public function getId(): int { return $this->attributes['id'];}
+    public function setId($id) : void { $this->attributes['id'] = $id; }
+
+    public function getName(): string { return $this->attributes['name']; }
+    public function setName($name) : void { $this->attributes['name'] = $name; }
+
+    public function getPrice(): int { return $this->attributes['price']; }
+    public function setPrice($price) : void { $this->attributes['price'] = $price;}
+
+
+}
